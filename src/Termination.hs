af2442427fd2ceef31f562d5995eb541233d5a22
Marty Stumpf <thealmartyblog@gmail.com>
2020-06-08 12:17:02 -0700

    Add matrix and semiring dep.

--- src/Termination.hs
+++ src/Termination.hs
@@ -1,8 +1,9 @@
 module Termination where
 
+-- order = {<,≤,?} when comparing an expression e to a pattern p
 data Order
   = Lt -- less than
   | Le -- less than or equal to
   | Un -- unknown
-  | Mat (Matrix Order) -- square matrices only
+  -- | Mat (Matrix Order) -- square matrices only
   deriving (Show, Eq, Ord)
