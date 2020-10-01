458f88b35f5d1519645b5548e122449149f21288
Marty Stumpf <thealmartyblog@gmail.com>
2020-10-01 12:52:21 -0700

    Start specialization function.

--- src/DecisionTrees.hs
+++ src/DecisionTrees.hs
@@ -2,27 +2,46 @@ module DecisionTrees where
 -- compiling function definitions with pattern matching to case trees
 -- inspired by 
 -- `Compiling Pattern Matching to Good Decision Trees` by Luc Maranget
-import Types (Pattern(WildCardP), Value )
-import Data.Matrix ( Matrix(nrows, ncols), getRow )
-import qualified Data.Vector as V (Vector, notElem, map)
+import Types (Pattern(..), Value )
+import Data.Matrix --( Matrix(nrows, ncols), getRow, matrix, (<|>), submatrix )
+import qualified Data.Vector as V --(Vector, notElem, map, head)
 
 -- matrices of clauses: P -> A, where P is the pattern matrices. A is the rhs.
 data ClauseMatrix
   = MkClauseMatrix (Matrix Pattern) (V.Vector Value)
+
+vJoin :: ClauseMatrix -> ClauseMatrix -> ClauseMatrix
+vJoin (MkClauseMatrix p1 v1) (MkClauseMatrix p2 v2) =
+  MkClauseMatrix ((<->) p1 p2) (v1 V.++ v2)
 
 -- matrix decomposition operations: 
 -- (1) specialization by a constructor c, S(c,P->A) 
 -- (2) computation of the default matrix, D(P->A)
 
 -- specialization by constructor c simplifies matrix P under the assumption that
--- v1 admits c as a head constructor.
-
-specialc :: 
-  Matrix Pattern -- pattern matrix P
-  -> V.Vector Value -- rhs values valuesV
-  -> Pattern -- constructor c
+-- v1 admits c as a head constructor. (See p.3 of paper)
+specialC :: 
+  Pattern -- specialization by this constructor (c)
+  -> Int -- the row to be checked
+  -> ClauseMatrix -- the clause matrix to be transformed
+  -- output is a matrix with some rows erased and some cols added
   -> ClauseMatrix
-specialc = undefined
+specialC c@(ConP name listP) i clauseM@(MkClauseMatrix p v) =
+  let firstRow = getRow i p in -- currently building the i-th row 
+  case V.head firstRow of
+    WildCardP -> -- when p_1^j is a wild card, add wild card cols to the row
+      let wildCardCols = 
+            -- the no. of cols to add equals the no. of constructor args
+            V.generate (length listP) (const WildCardP)
+          dropped1Col = -- drop the first col of the original row
+            V.slice 2 (ncols p - 1) firstRow
+      in
+        vJoin 
+          (MkClauseMatrix (rowVector (wildCardCols V.++ dropped1Col)) (V.take i v))
+          (specialC c (i+1) clauseM) -- TODO i needs to be less than nrows p
+    (ConP nameC listPat) -> undefined
+    _ -> undefined
+
 
 -- the default matrix retains the rows of P whose first pattern p_1^j admits all
 -- values c'(v1,...va) as instances, where constructor c' is not present in the
