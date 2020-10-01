4b06a65ddaa0e652fa1a2aa62b06912f65de8cf9
Marty Stumpf <thealmartyblog@gmail.com>
2020-10-01 10:44:48 -0700

    Add clause matrix data type.

--- src/DecisionTrees.hs
+++ src/DecisionTrees.hs
@@ -6,13 +6,23 @@ import Types (Pattern(WildCardP), Value )
 import Data.Matrix ( Matrix(nrows, ncols), getRow )
 import qualified Data.Vector as V (Vector, notElem, map)
 
--- matrices of clauses: P -> A, where P is the pattern matrices.
+-- matrices of clauses: P -> A, where P is the pattern matrices. A is the rhs.
+data ClauseMatrix
+  = MkClauseMatrix (Matrix Pattern) (V.Vector Value)
+
 -- matrix decomposition operations: 
 -- (1) specialization by a constructor c, S(c,P->A) 
 -- (2) computation of the default matrix, D(P->A)
 
 -- specialization by constructor c simplifies matrix P under the assumption that
 -- v1 admits c as a head constructor.
+
+specialc :: 
+  Matrix Pattern -- pattern matrix P
+  -> V.Vector Value -- rhs values valuesV
+  -> Pattern -- constructor c
+  -> ClauseMatrix
+specialc = undefined
 
 -- the default matrix retains the rows of P whose first pattern p_1^j admits all
 -- values c'(v1,...va) as instances, where constructor c' is not present in the
@@ -41,23 +51,21 @@ cc ::
   -- defines the fringe (the subterms of the subject value that  
   -- need to be checked against the patterns of P to decide matching)
   V.Vector Occurrence 
-  -> Matrix Pattern -- pattern matrix P
-  -> V.Vector Value -- rhs values valuesV
+  -> ClauseMatrix
   -> DTree
-cc oV pMatrix valuesV
-  | nrows pMatrix == 0 = Fail -- if there's no pattern to match it fails.
-  | ncols pMatrix == 0 || -- if the number of column is 0 or
+cc oV (MkClauseMatrix p a)
+  | nrows p == 0 = Fail -- if there's no pattern to match it fails.
+  | ncols p == 0 || -- if the number of column is 0 or
   -- if the first row of P has all wildcards 
     V.notElem 
       False 
-      (V.map (== WildCardP) (getRow 1 pMatrix)) 
+      (V.map (== WildCardP) (getRow 1 p)) 
         = Leaf 1 -- then the first action is yielded.
   | otherwise = -- P has at least one row and at least one column and 
   -- in the first row, at least one pattern is not a wild card
-      compiles oV pMatrix valuesV
+      compiles oV (MkClauseMatrix p a)
 
 compiles :: V.Vector Occurrence 
-  -> Matrix Pattern -- pattern matrix P
-  -> V.Vector Value -- rhs values valuesV
+  -> ClauseMatrix
   -> DTree
-compiles oV pMatrix valuesV = undefined
+compiles oV (MkClauseMatrix p a) = undefined
